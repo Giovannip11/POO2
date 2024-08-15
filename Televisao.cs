9ddb879cf69b9c3e466f989fbaf4586b0fa50479
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2024-08-15 14:37:32 -0300

    Update Televisao.cs

--- Televisao.cs
+++ Televisao.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace Aula1
 {
-    internal class Televisao
-    {
-
-        String Marca { get; set; }
+     String Marca { get; set; }
         int Tamanho { get; set; }
         String Tipo { get; set; }
 
@@ -19,9 +16,15 @@ namespace Aula1
             Tamanho = tamanho;
             Tipo = tipo;
         }
-
+        public void ligar(){
+            Console.Write("ligando...");
+        }
+        public void desligar(){
+            Console.Write("Desligando...");
+        }
+        public void mudarDeCanal(){
+            Console.Write("Mudando de canal...");
+        }
     }
-
 }
-
 
